9a79e9d7d1e82a85265be14f0d4422c5ed35e8d9
Robert Adams <robert@robertadams.net>
2015-06-24 22:46:37 -0700

    StatusView class.

---
+++ status_view.hpp
@@ -0,0 +1,35 @@
+#ifndef status_view_hpp_2015_06_24_1559
+#define status_view_hpp_2015_06_24_1559 
+
+#include "terminal.hpp"
+#include "view.hpp"
+#include <string>
+#include <functional>
+
+class StatusView final : public virtual View {
+public:
+  StatusView();
+  void set_content(std::string s) { internal_ = s; }
+  void set_external(const std::string& s) { content_ref_ = s; }
+  void set_internal() { content_ref_ = internal_; }
+  const std::string& content() const { return content_ref_; }
+private:
+  std::reference_wrapper<const std::string> content_ref_;
+  std::string internal_;
+  virtual void v_show() override {
+    auto view_rect = Terminal::size();
+    Terminal::locate(view_rect.left(), view_rect.bottom());
+    std::string s = content();
+    auto print_len = 
+      content().size() > view_rect.width() ? 
+      view_rect.width() : content().size();
+
+    Terminal::write(s.c_str(), s.size());
+  }
+};
+
+inline 
+StatusView::StatusView() : content_ref_(internal_) {
+}
+
+#endif//status_view_hpp_2015_06_24_1559
