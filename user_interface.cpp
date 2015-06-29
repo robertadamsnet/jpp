923ca7f65dfe6be6a5bced424cb90af9d7d5ca7f
Robert Adams <robert@robertadams.net>
2015-06-28 18:25:12 -0700

    Added controller reference; completely changed interface.

--- user_interface.cpp
+++ user_interface.cpp
@@ -6,11 +6,112 @@
 
 */
 #include "user_interface.hpp"
+#include "scoped_call.hpp"
+#include "editor.hpp"
+#include "controller.hpp"
 
-UserInterface::UserInterface() {
+UserInterface::UserInterface(Controller& c) 
+  : controller_(c) 
+{
   Terminal::no_echo();
 }
 
-void UserInterface::v_show() {
-  //action_view_.show();  
+const StatusView& UserInterface::status() const {
+  return status_view_;
+}
+
+void UserInterface::hard_coded_menu() {
+  typedef std::vector<std::string> msg_stack_type;
+  
+  std::function<void(std::function<void(int)>)> get_input;
+ 
+  msg_stack_type msg_stack;
+  
+  enum class Result {
+    Ok,
+    Cancel,
+    Back
+  };
+
+  typedef std::function<void(int)> switch_fn_type;
+
+  auto combined_message = [&]() { 
+    std::string message;
+    for(const auto& s : msg_stack) {
+      message += "[" + s + "]";
+    }
+    return message;
+  };
+
+  auto push_status = [&](const std::string& msg) {
+    msg_stack.push_back(msg);
+    status_view_.set_content(combined_message());
+    status_view_.refresh();
+  };
+
+  auto pop_status = [&]() {
+    msg_stack.pop_back();
+    status_view_.set_content(combined_message());
+    status_view_.refresh();
+  };
+
+  auto action = [&](std::function<void(void)> action, std::string name) {
+    push_status(name);
+    action();
+    pop_status();
+  };
+
+  auto submenu_action = [&](std::function<void(int)> action, 
+      std::string name) 
+  {
+    push_status(name);
+    get_input(action);
+    pop_status();
+  };
+
+  switch_fn_type create_menu = [&](int key) {
+    switch(key) {
+    case 'c':
+      action(std::bind(&UserInterface::create_class, this), "Class");
+    }
+  };
+
+  switch_fn_type top_menu = [&](int key) 
+  {
+    switch(key) {
+    case 'c':
+      submenu_action(create_menu, "Create");
+    }
+  };
+
+  get_input = [&](std::function<void(int)> key_processor) 
+  {
+    int key = Terminal::getch(); 
+    key_processor(key);
+  };
+    
+  while(true) {
+    submenu_action(top_menu, ">");
+  }
+}
+
+
+void UserInterface::create_class() {
+  Object obj;
+  std::string& name = obj.get<Object::Name>();
+
+  int column = status().content().size();
+  int row = status().row();
+  int width = Terminal::rect().width() - column;
+  Editor e(row, column, width, "Name: ");
+  auto result = e.activate();  
+}
+
+void UserInterface::v_refresh() {
+  hard_coded_menu();
+}
+
+Controller& UserInterface::controller() const
+{
+  return controller_;
 }
--- user_interface.hpp
+++ user_interface.hpp
@@ -11,114 +11,31 @@
 class StatusView;
 
 #include "status_view.hpp"
+#include "tree_view.hpp"
 
 #include <map>
+#include <vector>
 #include <memory>
-
-class Action {
-public:
-  enum Result {
-    Ok,
-    Cancel,
-    Back,
-    ErrorReset
-  };
-  
-  const std::string& name() const { return v_name(); }
-  std::function<Result(void)> action() const { return v_action(); }
-private:
-  virtual const std::string& v_name() const = 0;
-  virtual std::function<Result(void)> v_action() const = 0;
-};
-
-class ActionItem final : public virtual Action {
-private:
-  std::string name_;
-  std::function<Result(void)> action_;
-  const std::string& v_name() const override;
-  std::function<Result(void)> v_action() const override;
-};
-
-inline
-std::function<Action::Result(void)> ActionItem::v_action() const {
-  return action_;
-}
-
-inline
-const std::string& ActionItem::v_name() const {
-  return name_;
-}
-
-typedef std::map<int, std::unique_ptr<Action>> ActionMap;
-
-class ActionView final : public virtual View, public virtual Action {
-public:
-  ActionView(std::string, const ActionMap&);
-private:
-  std::string name_;
-  std::function<Result(void)> action_;
-  const ActionMap& map_;
-  void v_show() override;
-  const std::string& v_name() const override;
-  std::function<Result(void)> v_action() const override; 
-  Result do_action();
-};
-
-inline 
-ActionView::ActionView(std::string n, const ActionMap& m) 
-: map_(m), name_(n)
-{
-}
+#include <stack>
+#include <functional>
 
 
-inline 
-void ActionView::v_show() {
-}
-
-inline
-ActionView::Result ActionView::do_action() {
-  StatusView status;
-  status.set_content(name());
-  status.show();
-  ActionMap::iterator item_pos;
-  while(true) {
-    int ch = Terminal::getch();
-    ActionMap::const_iterator found_item = map_.find(ch);
-    if(found_item != map_.end()) {
-      auto fn = found_item->second;
-      auto result = fn();
-      switch(result) {
-      case Ok:
-        return Ok;
-      case Cancel:
-        break;
-      case Back:
-        return Cancel;
-      case ErrorReset:
-        return ErrorReset;
-      }
-    }
-  }
-}
-
-inline
-const std::string& ActionView::v_name() const {
-  return name_;
-};
-
-inline
-std::function<Action::Result(void)> ActionView::v_action() const {
-  return action_;
-}
+class Controller;
 
 class UserInterface final : public virtual View {
 public:
-  UserInterface();
-  const StatusView& status();
+  UserInterface(Controller&);
+  const StatusView& status() const;
+
+  void create_class();
+
+  Controller& controller() const;
 private:
+  Controller& controller_;
   StatusView status_view_;
-  void v_show() override; 
-    
+  TreeView tree_view_;
+  void v_refresh() override;  
+  void hard_coded_menu();
 };
 
 
